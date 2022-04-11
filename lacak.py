5489d110ed624f6bd8c17534c2c44c0ca901f775
Nurminxv <103430925+nurminxv@users.noreply.github.com>
2022-04-12 01:36:04 +0700

    Add files via upload

---
+++ lacak.py
@@ -0,0 +1,18 @@
+import os
+
+os.system('clear')
+print('\033[33;1m')
+print('   [•]  Author          : Nurmin')
+print('   [•]  Youtube         : Fakta Semata')
+print('   [•]  Github          : katataaa')
+print('   [•]  Facebook        : leader')
+print('\n'+'----------------------------------------->')
+print('\033[36;1m')
+print('   [1]. lacak handpone dengan gmail & riset')
+print('   [0]. exit')
+print('\033[37;1m')
+option=int(input('   [•]. silahkan pilih opsinya:'))
+if option==1:
+        print('silahkan ikuti langkah nya')
+        import os
+os.system('xdg-open https://myaccount.google.com/find')
